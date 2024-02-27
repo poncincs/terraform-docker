e94d650d690c35e6bc629d6c3572f38e4a4e1c3d
poncincs <samuel.poncin-chaperon@mines-ales.org>
2024-02-27 16:45:49 +0100

    modif main and add dir module

--- main.tf
+++ main.tf
@@ -1,56 +1,13 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
-  }
-}
+module "docker-nginx" {
+    source = "./module/docker-nginx"
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
-# Create a docker image resource
-resource "docker_image" "image" {
-  name         = var.container_image
-  keep_locally = true
-}
-
-# Create a docker volume resource
-resource "docker_volume" "volume" {
-  count = var.containers_number
-  name  = "${var.container_volume}-${count.index}"
-}
-
-resource "docker_container" "container" {
-  count = var.containers_number
-  name  = "${var.container_name}-${count.index}"
-  hostname = "${var.container_name}-${count.index}"
-  image = docker_image.image.image_id
-  memory = var.container_memory
-  privileged = var.container_privileged
-
-  ports {
-    external = var.ext_port + count.index
-    internal = var.int_port
-  }
-
-  volumes {
-    volume_name    = docker_volume.volume[count.index].name
-    container_path = "/usr/share/nginx/html"
-    read_only      = false
-  }
-
-  healthcheck {
-    test = var.healthcheck_command
-    interval = "3s"
-    retries = 0
-  }
-  
-  command = [
-    "sh",
-    "-c",
-    "echo '<html><head><title>${var.container_name}-${count.index}</title></head><body><h1>Container Hostname: ${var.container_name}-${count.index}</h1></body></html>' > /usr/share/nginx/html/index.html && nginx -g 'daemon off;'"
-  ]
+    container_name      = "nginx-app"
+    container_image     = "nginx:latest"
+    containers_number   = 3
+    int_port            = 80
+    ext_port            = 3000
+    container_volume    = "nginx-app_data"
+    container_memory    = 256
+    container_privileged = false
+    healthcheck_command = ["CMD", "curl", "-f", "localhost"]
 }
